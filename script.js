aa928988cfa0cdd8f1351e06431eb2f31c34ae34
ZalynZann <82177693+ZalynZann@users.noreply.github.com>
2023-06-12 19:08:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,64 @@ $closeBtnsArr.forEach(function($btn) {
 });
 
 
+//Chatbot
+var chatContainer = document.getElementById('chat-container');
+var chatToggleButton = document.getElementById('chat-toggle-btn');
+
+function toggleChat() {
+  chatContainer.style.display = chatContainer.style.display === 'none' ? 'block' : 'none';
+}
+
+function sendMessage() {
+  var userMessage = document.getElementById('user-message').value;
+  appendMessage('user', userMessage);
+
+  // Process the user's message and generate the bot's reply
+  var botReply = getBotReply(userMessage);
+
+  // Simulating bot typing delay for a more realistic chat experience
+  setTimeout(function() {
+    appendMessage('bot', botReply);
+  }, 1000);
+
+  document.getElementById('user-message').value = '';
+}
+
+// Add event listener for the enter key press
+document.getElementById('user-message').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevents the default behavior of the enter key (form submission)
+
+    sendMessage(); // Call the sendMessage function when the enter key is pressed
+  }
+});
+
+function appendMessage(sender, message) {
+  var chatDisplay = document.getElementById('chat-display');
+  var messageElement = document.createElement('div');
+  messageElement.className = 'message ' + sender;
+  messageElement.textContent = message;
+  chatDisplay.appendChild(messageElement);
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+}
+
+function getBotReply(message) {
+  // You can customize the chatbot's responses based on the user's message
+
+  const lowerCaseMessage = message.toLowerCase();
+
+  if (lowerCaseMessage.includes("hello")) {
+    return "Hello! How can I assist you today?";
+  } else if (lowerCaseMessage.includes("weather")) {
+    return "The weather is sunny and warm.";
+  } else if (lowerCaseMessage.includes("help")) {
+    return "Sure, I'm here to help! What do you need assistance with?";
+  } else if (lowerCaseMessage.includes("hi")) {
+    return "Hello! How can I assist you today?";
+  } else if (lowerCaseMessage.includes("firstkey")) {
+    return "FirstKey is a business setup services in Dubai";
+  } 
+  // For this simple example, the bot will just echo the user's message
+  return message;
+}
+
